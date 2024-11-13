08080679fe01f3a2968dacf4e45afe5d31a32c68
waqar-ahmad356 <flashdrivepunjab@gmail.com>
2024-11-13 20:06:08 +0500

    development-mode

---
+++ script.js
@@ -0,0 +1,6 @@
+function toggleMenu() {
+    const menu = document.getElementById("navbar-menu");
+    const button = document.getElementById("nav-btn");
+    menu.classList.toggle("active");
+    button.classList.toggle("active");
+  }
